4b631b093af64b2442659c55592db0e5433140c2
Dennis Fricke <dennis.fricke@ninjaneers.de>
2017-11-09 09:52:03 +0100

    Create reporter for owasp

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# nsp-reporter-remarkup
+# nsp-reporter-owasp
 
-> Summary-style reporter for nsp (Node Security Project), using [Remarkup table syntax](https://secure.phabricator.com/book/phabricator/article/remarkup/#tables)
+> Summary-style reporter for nsp (Node Security Project), to generate owasp dependency check
 
 See [nsp](https://github.com/nodesecurity/nsp/tree/3.0.0) on Github for more information.
--- src/reporter.js
+++ src/reporter.js
@@ -1,59 +1,76 @@
 'use strict';
 
-var Cvss = require('Cvss');
+var builder = require('xmlbuilder');
 
 exports.error = function (err, args) {
-	console.error(err.message);
+    console.error(err.message);
 };
 
-// Purposely do nothing
 exports.success = function (result, args) {
+    console.log(result);
 };
 
 exports.check = {};
 exports.check.success = function (result, args) {
 
-	var pipeWrap = function (items) {
-		return '|' + items.join('|') + '|';
-	};
+    var dependencies = [];
+    var project = 'unknown';
 
-	var header = [
-		'Severity',
-		'Title',
-		'Module',
-		'Installed',
-		'Patched',
-		'Include Path',
-		'More Info'
-	];
+    result.data.forEach(function (element) {
+        project = element.path[0];
 
-	var rows = [];
-	rows.push( pipeWrap(header) );
-	rows.push( pipeWrap( Array(header.length).fill('--') ) );
+        var dependency = {
+            dependency: {
+                '@isVirtual': 'false',
+                fileName: element.module + '@' + element.version,
+                filePath: element.module + '@' + element.version,
+                md5: '',
+                sha1: '',
+                description: element.title,
+                identifiers: {
+                    identifier: {
+                        name: element.module,
+                        '@type': 'nsp'
+                    }
+                },
+                vulnerabilities: [{
+                    vulnerability: {
+                        '@source': 'NSP',
+                        name: element.advisory,
+                        cvssScore: element.cvss_score,
+                        cvssAccessVector: element.cvss_vector,
+                        cvssAccessComplexity: 'LOW',
+                        cvssAuthenticationr: 'NONE',
+                        cvssConfidentialImpact: 'NONE',
+                        cvssIntegrityImpact: 'NONE',
+                        cvssAvailabilityImpact: 'PARTIAL',
+                        severity: 'Low',
+                        description: element.overview + '||' + element.recommendation + ' vulnerable versions: ' + element.vulnerable_versions +
+                        ' patched_versions: ' + element.patched_versions
+                    }
+                }]
+            }
+        };
 
-	result.data.forEach( (finding) => {
-		var advisory_number
-			= finding.advisory.substr(finding.advisory.lastIndexOf('/'));
+        dependencies.push(dependency);
+    });
 
-		// Pipe characters cannot be escaped within
-		// Remarkup tables so replace with OR
-		var patched_versions
-			= finding.patched_versions === '<0.0.0'
-				? 'None'
-				: finding.patched_versions.replace(/\|\|/g, 'OR');
+    var result = {
+        analysis: {
+            '@xmlns': 'https://jeremylong.github.io/DependencyCheck/dependency-check.1.6.xsd',
+            scanInfo: {
+                engineVersion: '3.0.1'
+            },
+            projectInfo: {
+                name: project,
+                reportDate: new Date().toLocaleString(),
+                credits: 'nsp'
+            },
+            dependencies: dependencies
+        }
+    };
 
-		rows.push(
-			pipeWrap([
-				Cvss.getRating(finding.cvss_score),
-				finding.title,
-				finding.module,
-				finding.version,
-				patched_versions,
-				'{nav ' + finding.path.join(' > ') + '}',
-				'[[' + finding.advisory + '|nspa' + advisory_number + ']]'
-			])
-		);
-	});
+    var xmlResult = builder.create(result, { encoding: 'utf-8', separateArrayItems: true });
+    console.log(xmlResult.end({ pretty: true }));
 
-	console.log( rows.join('\n') );
 };
